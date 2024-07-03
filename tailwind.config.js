f4cabca2c07570fce7c659088a072a97756cac30
iftakhar <iftakharalamshuvo11@gmail.com>
2024-07-04 02:40:13 +0600

    service added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
             "textheading":'#020043',
             "btncolor":'#FFC637',
             "ptextcolor":"#4D4C7B",
-            "sectioncolor":"FFFFF5"
+            "sectioncolor":"#FFFFF5"
           },
          
         },
