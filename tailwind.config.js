c95f9af62ed34eb9b6fa029a9369b1ce54aa09d4
iftakhar <iftakharalamshuvo11@gmail.com>
2024-07-04 00:05:23 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+          colors:{
+            "textheading":'#020043',
+            "btncolor":'#FFC637',
+            "ptextcolor":"#4D4C7B",
+            "sectioncolor":"FFFFF5"
+          },
+         
+        },
+        fontFamily:{
+          oswald:[ "Oswald", "sans-serif"],
+          inter:[  "Inter", "sans-serif"]
+          
+        },
+      },
+    plugins: [
+        
+      ],
+  }
